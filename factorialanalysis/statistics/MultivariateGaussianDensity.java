7d51d3e5157ad81a6302ea51d5f8a4a376ba497a
Emmanuel <emmanuel.salcido.maldonado@gmail.com>
2017-01-05 00:18:12 -0600

    class MultivariateGaussianDensity added

---
+++ factorialanalysis/statistics/MultivariateGaussianDensity.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorialanalysis.statistics;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class MultivariateGaussianDensity {
+    
+    private MVarianceCovariance covarianzeMatrix;
+    private Mean meanVector;
+    private double[][] matrix;
+    
+}
